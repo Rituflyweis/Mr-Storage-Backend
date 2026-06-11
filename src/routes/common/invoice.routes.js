e7349693085d988f7c9ddfffdeadeb4e88b42576
Rituflyweis <ritu@flyweis.technology>
2026-06-11 17:46:10 +0530

    issue fixed

--- src/routes/common/invoice.routes.js
+++ src/routes/common/invoice.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { body } = require('express-validator')
+const { body ,query} = require('express-validator')
 const validate = require('../../middleware/validate')
 const ctrl = require('../../controllers/common/invoice.controller')
 
