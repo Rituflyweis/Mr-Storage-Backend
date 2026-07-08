6b9d31ba18db149692a695d3afaf0e66ae434ff1
Rituflyweis <ritu@flyweis.technology>
2026-07-08 18:11:08 +0530

    add delivery routes

--- src/routes/admin/plant/index.js
+++ src/routes/admin/plant/index.js
@@ -15,5 +15,6 @@ router.use('/projects', require('./projectOps.routes'))
 router.use('/vendors', require('./vendor.routes'))
 router.use('/carriers', require('./carrier.routes'))
 router.use('/smdt', require('./smdt.routes'))
+router.use('/deliveries', require('./delivery.routes'))
 
 module.exports = router
